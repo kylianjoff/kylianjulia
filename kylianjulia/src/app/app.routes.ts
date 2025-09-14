93a5f587936090cb3daa85bac0dd3c7cdc9dba1d
kylianjoff <kylianju382@gmail.com>
2025-09-15 00:35:08 +0200

    Page d'accueil

--- kylianjulia/src/app/app.routes.ts
+++ kylianjulia/src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 import { Blog } from './blog/blog';
 import { Projects } from './projects/projects';
+import { Home } from './home/home';
 
 export const routes: Routes = [
+    {
+        path: '',
+        title: "Bienvenue sur le site de Kylian JULIA",
+        component: Home,
+        pathMatch: 'full'
+    },
     {
         path: 'blog',
         title: "Blog de Kylian JULIA",
