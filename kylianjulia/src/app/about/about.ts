4d933088cf455b20385b681f401e329ed31b61ca
kylianjoff <kylianju382@gmail.com>
2025-10-09 13:47:50 +0200

    ouverture/fermeture menu about

--- kylianjulia/src/app/about/about.ts
+++ kylianjulia/src/app/about/about.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './about.html',
   styleUrl: './about.css'
 })
 export class About {
-  
+  menuCV: boolean = false;
+
+  toggleMenuCV() {
+    this.menuCV = !this.menuCV;
+  }
 }
