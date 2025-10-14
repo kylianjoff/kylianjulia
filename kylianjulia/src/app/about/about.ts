df725d21960453dd2f7dea64334b1ff959a29cc1
kylianjoff <kylianju382@gmail.com>
2025-10-14 09:36:45 +0200

    Section associatif

--- kylianjulia/src/app/about/about.ts
+++ kylianjulia/src/app/about/about.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-import { NgIf } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
+interface Association {
+  name: string;
+  description: string;
+  role: string;
+  logo: string;
+  details?: string[];
+}
 
 @Component({
   selector: 'app-about',
-  imports: [NgIf],
+  imports: [CommonModule],
   templateUrl: './about.html',
   styleUrl: './about.css'
 })
@@ -13,5 +20,61 @@ export class About {
 
   toggleMenuCV() {
     this.menuCV = !this.menuCV;
+  }
+
+  associations: Association[] = [
+    {
+      name: 'Image',
+      description: 'Junior entreprise de l\'ISIMA',
+      role: 'Vice-président (10/2025 - actuel)',
+      logo: 'assos/image.png',
+      details: [
+        'Aide au relancement de l\'association'
+      ]
+    },
+    {
+      name: 'DreZZing',
+      description: 'Association de textile et goodies de l\'ISIMA',
+      role: 'Trésorier (02/2025 - actuel)',
+      logo: 'assos/drezzing.png',
+      details: [
+        'Assurer la stabilité financière de l\'association',
+        'Gestion des projets'
+      ]
+    },
+    {
+      name: 'Shared',
+      description: 'Club caritatif de l\'ISIMA',
+      role: 'Responsable de la communication interne (03/2025 - actuel)',
+      logo: 'assos/shared.png',
+      details: [
+        'Membre fondateur du club',
+        'Communication des actions caritatives auprès des étudiants de l\'ISIMA',
+        'Travail avec des associations',
+        'Organisation de la partie associative de l\'événement ZZevent organisé par le BDE ISIMA'
+      ]
+    },
+    {
+      name: 'Isimalt',
+      description: 'Club de bière de l\ISIMA',
+      role: 'Responsable de la communication (02/2025 - actuel)',
+      logo: 'assos/isimalt.jpg'
+    },
+    {
+      name: 'Sigmix',
+      description: 'Association de mix de Sigma Clermont et ISIMA',
+      role: 'DJ (05/2025 - actuel)',
+      logo: 'assos/sigmix.JPEG',
+      details: [
+        'Débutant',
+        'Assurer l\'ambiance des soirées'
+      ]
+    }
+  ]
+
+  selectedAsso?: Association;
+
+  selectAsso(asso: Association) {
+    this.selectedAsso = asso;
   }
 }
