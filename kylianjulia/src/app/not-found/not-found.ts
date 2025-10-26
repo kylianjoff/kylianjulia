ad4c6b40f5ac2feffc061771194e38bf2a55fe6b
kylianjoff <kylianju382@gmail.com>
2025-10-26 22:06:21 +0100

    page NotFound

--- kylianjulia/src/app/not-found/not-found.ts
+++ kylianjulia/src/app/not-found/not-found.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './not-found.html',
   styleUrl: './not-found.css'
 })
 export class NotFound {
-
+  constructor() {
+    // Optionnel : log pour debug
+    console.log('404 - Page not found');
+  }
 }
