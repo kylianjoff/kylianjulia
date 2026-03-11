fdcb04114a893e5cdc430e8a719baa25d33520d0
kylianjoff <kylianju382@gmail.com>
2026-03-11 18:56:57 +0100

    Création du composant et début de l'import sur la page principale

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { Contact } from '../contact/contact';
 import { RouterLink } from '@angular/router';
 
 interface BlogPost {
@@ -35,7 +35,7 @@ interface ConsoleLine {
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, Contact],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
